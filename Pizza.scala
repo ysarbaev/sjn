0630db443d4365a60d3a9c32fb3a1eff7d9516f3
Yuri Sarbaev <yuri.sarbaev@gmail.com>
2016-01-10 21:43:33 +0300

    it seems to be ok, optimization needed

--- Pizza.scala
+++ Pizza.scala
@@ -1,6 +1,7 @@
 import io.Source
+import scala.annotation.tailrec
 
-object Pizza extends App {
+object Pizza {
 	
 	val MIN_ORDERS = 0
 
@@ -30,10 +31,7 @@ object Pizza extends App {
 
 	case class Order(time: Int, size: Int)
 
-	init(args)
-
-	def init(args: Array[String]): Unit = {
-		
+	def main(args: Array[String]): Unit = {
 
 		if(args.contains(DEBUG_OPT)) {
 			DEBUG = true
@@ -44,22 +42,31 @@ object Pizza extends App {
 			System.exit(0)
 		}
 
+		val result = process(Source.stdin.getLines.toList)
 
+		println(result)
 
 	}
 
 	def printHelp() {
 		println(
 			"""
-				It calculates mininimal averege waiting time for queue.
-				Input data format: 
-
-			"""
+				|It calculates mininimal averege waiting time for queue.
+				|Input data format:
+				|N - total number of orders
+				|T1  L1 - the moment of time when customer has entered and time to cook this pizza
+				|....
+				|Tn  Ln
+				|
+				|Options:
+				|--help - prints this document
+				|--debug - more verbose error info
+			""".stripMargin
 		)
 	}
 
 	def process(data: List[String]): Int = {
-		val queueSize = data.headOption.getOrElse(throw new Exception("No data")).toInt
+		val queueSize = data.headOption.getOrElse(throw new Exception("No data")).trim.toInt
 		if(! isOrdersQueueSizeValid(queueSize)) {
 			throw new Exception("Enter order size $MAX_ORDERS <= N >= $MIN_ORDERS")
 		}
@@ -68,9 +75,34 @@ object Pizza extends App {
 			throw new Exception("Orders queue size != $queueSize")
 		}
 
-		val orders = ordersRaw.map(parseOrder)
+		val orders = ordersRaw.map(parseOrder).sortBy(_.time)
 
-		
+		if(queueSize == 0){
+			0
+		} else {
+			val first = orders.head
+			val total = calcTotalWaitingTime(orders.tail, first.size, first.time + first.size)
+			(total / queueSize).toInt
+		}
+	}
+
+	@tailrec
+	def calcTotalWaitingTime(rest: List[Order], accWaitingTime: Int, currentTime: Int): Int = {
+		if (rest.isEmpty) {
+			accWaitingTime
+		} else {
+			val (next, nextWT) = {
+				val waitingOrders = rest.filter(_.time <= currentTime)
+				if (waitingOrders.isEmpty) {
+					(rest.head, rest.head.size)
+				} else {
+					val min = waitingOrders.minBy(_.size)
+					(min, (currentTime - min.time) + min.size)
+				}
+			}
+			val todoList = rest.filter(_.time != next.time)
+			calcTotalWaitingTime(todoList, accWaitingTime + nextWT, currentTime + nextWT)
+		}
 	}
 
 	def orderErr(data: String) = throw new Exception(s"Invalid order: $data")
