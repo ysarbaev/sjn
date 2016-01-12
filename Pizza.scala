9bdd8c64ab2e5d8ba31de78fa6430a2d6b1563a0
Yuri Sarbaev <yuri.sarbaev@gmail.com>
2016-01-12 23:30:21 +0300

    bugfixing, use BugInt, more samples

--- Pizza.scala
+++ Pizza.scala
@@ -26,7 +26,7 @@ object Pizza {
 
 	def isPizzaSizeValid(size: Int) = size >= MIN_PIZZA_SIZE && size <= MAX_PIZZA_SIZE
 
-	case class Order(time: Int, size: Int)
+	case class Order(time: BigInt, size: BigInt)
 
 	def main(args: Array[String]): Unit = {
 
@@ -41,11 +41,11 @@ object Pizza {
 	def process(data: List[String]): Int = {
 		val queueSize = data.headOption.getOrElse(throw new Exception("No data")).trim.toInt
 		if(! isOrdersQueueSizeValid(queueSize)) {
-			throw new Exception("Enter order size $MAX_ORDERS <= N >= $MIN_ORDERS")
+			throw new Exception(s"Enter order size $MAX_ORDERS <= N >= $MIN_ORDERS")
 		}
 		val ordersRaw = data.tail
 		if(ordersRaw.size != queueSize) {
-			throw new Exception("Orders queue size != $queueSize")
+			throw new Exception(s"Orders queue size expected $queueSize, but ${ordersRaw.size}")
 		}
 
 		val orders = ordersRaw.map(parseOrder)
@@ -57,7 +57,7 @@ object Pizza {
 		}
 	}
 
-	def calcTotalWaitingTime(orders: List[Order]): Int = {
+	def calcTotalWaitingTime(orders: List[Order]): BigInt = {
 		val first = orders.head
 		val tree = SortedMap(orders.map(o => o.time -> o): _*) - first.time
 		calcTotalWaitingTimeRec(tree, first.size, first.time + first.size)
@@ -67,7 +67,7 @@ object Pizza {
 	//situation it will require O(NLogN) operations, however in the worst 
 	//case it will be O(N^2)
 	@tailrec
-	def calcTotalWaitingTimeRec(tree: SortedMap[Int, Order], accWaitingTime: Int, currentTime: Int): Int = {
+	def calcTotalWaitingTimeRec(tree: SortedMap[BigInt, Order], accWaitingTime: BigInt, currentTime: BigInt): BigInt = {
 		if (tree.isEmpty) {
 			accWaitingTime
 		} else {
@@ -81,8 +81,8 @@ object Pizza {
 					(min, (currentTime - min.time) + min.size)
 				}
 			}
-			val todoList = tree - next.time
-			calcTotalWaitingTimeRec(todoList, accWaitingTime + nextWT, currentTime + nextWT)
+			val todo = tree - next.time
+			calcTotalWaitingTimeRec(todo, accWaitingTime + nextWT, currentTime + nextWT)
 		}
 	}
 
@@ -109,29 +109,29 @@ object Pizza {
 	def test() {
 		import scala.util.Random
 		def genOrders(orders: Int, maxTimeDistance: Int, maxSize: Int): List[Order] = {
-			var i = 0
+			var i = 10
 			(0 to orders).map { j =>
-				val o = Order(i, Random.nextInt(maxSize))
-				i += Random.nextInt(maxTimeDistance)
+				val o = Order(i, Random.nextInt(maxSize) + 1)
+				i += Random.nextInt(maxTimeDistance) + 1
 				o
 			}.toList
 		}
 
-		def time(msg: String)(block: => Unit) {
+		def time(vol: Int)(block: Int => Any) {
 			val t = System.currentTimeMillis()
-			block
+			val result = block(vol)
 			val tt = (System.currentTimeMillis() - t) / 1000
-			println(s"$msg, time = $tt")
+			println(s"$vol, time = $tt, $result")
 		}
 
-		time("1000") {
-			calcTotalWaitingTime(genOrders(1000, 100, 1000))
+		time(1000) { vol =>
+			calcTotalWaitingTime(genOrders(vol, 100, 1000)) / vol
 		}
-		time("10000") {
-			calcTotalWaitingTime(genOrders(10000, 100, 1000))
+		time(10000) { vol =>
+			calcTotalWaitingTime(genOrders(vol, 100, 1000)) / vol
 		}
-		time("20000") {
-			calcTotalWaitingTime(genOrders(20000, 100, 1000))
+		time(20000) { vol =>
+			calcTotalWaitingTime(genOrders(vol, 100, 1000)) / vol
 		}
 	}
 }
